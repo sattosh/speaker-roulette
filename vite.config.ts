5be09df17af77bf37e21370b9c2a87295796158f
HiroshimaSatoshi <hirosh@wni.com>
2025-06-13 11:22:01 +0900

    fix: pathが間違ってたので修正

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/speaker_roulette/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/speaker-roulette/' : '/',
   test: {
     globals: true,
     environment: 'jsdom',
